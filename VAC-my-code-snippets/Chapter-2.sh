bd72f3ac758f782706ad43eb640cdbf2527a1a52
vasilecondrea <vasile.condrea@gmail.com>
2023-02-22 18:16:13 +0000

    added process managemen and memory management chapter 2

--- VAC-my-code-snippets/Chapter-2.sh
+++ VAC-my-code-snippets/Chapter-2.sh
@@ -11,7 +11,6 @@ dmidecode
 echo "
     if you only want to see the architecture (x86_64 for me) do uname -m
 "
-
 uname -m
 
 echo "
@@ -19,3 +18,26 @@ echo "
     uname -srm
 "
 uname -srm
+
+echo "
+    see the sessions, process groups, and processes active at a particular time; 
+            abbreviations:
+    ps - Process Status
+    TTY is the terminal type and this is what the user is logged into.
+    TIME - is the total amount of the CPU both in minutes and seconds that the process has been running
+    CMD - is the name of the command line that carried out the process
+    PID - process id
+    PGID - process group id
+"
+    ps -j
+
+echo "
+     A very useful tool to figure out memory-related information such as how much RAM is available to you is the /proc/meminfo interface.
+     how to list the ram size:   grep MemTotal /proc/meminfo (8GB)
+     how to see the size of the virtual memory (34TB)
+     how to see huge pages information (2MB)
+"
+grep MemTotal /proc/meminfo
+grep VmallocTotal /proc/meminfo
+grep Huge /proc/meminfo
+
